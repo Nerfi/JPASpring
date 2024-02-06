f39805da66df7d3cece86ebb12bd54825131bdfa
juan <juanchuu.jfpc@gmail.com>
2024-02-06 14:04:46 +0100

    added my project

---
+++ src/main/java/JPA/JPA/JpaApplication.java
@@ -0,0 +1,14 @@
+package JPA.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("JUANANANNAN");
+		SpringApplication.run(JpaApplication.class, args);
+	}
+
+}
