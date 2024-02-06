a2b6b874709a4f26aac1d0b4498934e032f4944d
juan <juanchuu.jfpc@gmail.com>
2024-02-06 14:46:41 +0100

    ignored

--- src/main/java/JPA/JPA/JpaApplication.java
+++ src/main/java/JPA/JPA/JpaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("JUANANANNAN");
+
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
